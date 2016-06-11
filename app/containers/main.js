9c0a8f4dcebda66647088c7846766ca0eef1b287
sjafri5 <raza.jafri.sy@gmail.com>
2016-06-11 20:21:47 +0300

    connected main component to store, passed in router

--- app/containers/main.js
+++ app/containers/main.js
@@ -1,36 +1,14 @@
 import React, { Component } from 'react';
-import { bindActionCreators } from 'redux';
 import { createStore, applyMiddleware, combineReducers  } from 'redux';
-import { connect  } from 'react-redux';
 import { Provider } from 'react-redux';
+import Router from './router'
 import thunk from 'redux-thunk';
 const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
 import * as reducers from '../reducers/index';
 const reducer = combineReducers(reducers);
 const store = createStoreWithMiddleware(reducer);
 
-import Styles from './../stylesheets/home-styles';
-import * as homeActions from '../actions/home-actions';
 
-import {
-  Text,
-  TextInput,
-  View
-} from 'react-native';
-
-const mapStateToProps = (state) => {
-  return {
-    //login: state.login,
-  }
-}
-
-const mapDispatchToProps = (dispatch) => {
-  return {
-    receiveWordInput: (text)=>{
-      dispatch(homeActions.receiveWordInput(text))
-    },
-  }
-}
 class Wordcase extends Component {
   handleWordInput(event){
     let { receiveWordInput } = this.props;
@@ -41,23 +19,10 @@ class Wordcase extends Component {
   render() {
     return (
       <Provider store={store}>
-        <View>
-          <Text>
-            Welcome to WordCase
-          </Text>
-          <TextInput
-            style={Styles.numericInputField}
-            placeholder={'enter word'}
-            value={''}
-            onChange= {this.handleChangeEvent}
-          />
-        </View>
+        <Router />
       </Provider>
     );
   }
 }
 
-module.exports = connect(
-                   mapStateToProps,
-                   mapDispatchToProps
-                 )(Wordcase);
+module.exports = Wordcase;
