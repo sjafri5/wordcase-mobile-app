a7285d6e48643ba142376e1a1a3e340f62eb962a
sjafri5 <raza.jafri.sy@gmail.com>
2016-06-11 17:16:25 +0300

    initial commit

---
+++ app/containers/main.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+import {
+  Text,
+  View
+} from 'react-native';
+
+class Wordcase extends Component {
+  render() {
+    return (
+      <View>
+        <Text>
+          Welcome to WordCase
+        </Text>
+      </View>
+    );
+  }
+}
+
+module.exports = Wordcase;
---
+++ index.android.js
@@ -0,0 +1,7 @@
+'use strict';
+
+import React, { Component } from 'react';
+import Main from './app/containers/main';
+import { AppRegistry } from 'react-native';
+
+AppRegistry.registerComponent('Wordcase', () => Main);
