57a0f9a3b2a4547581e506f29b848b153aec9eef
sjafri5 <raza.jafri.sy@gmail.com>
2016-06-11 20:21:07 +0300

    added reducer index and home reducer

---
+++ app/reducers/home-reducer.js
@@ -0,0 +1,12 @@
+import * as types from '../actions/action-types';
+
+const initialState = {
+  word: ''
+}
+
+export default function home(state = initialState, action = {}) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
---
+++ app/reducers/index.js
@@ -0,0 +1,3 @@
+import home from './home-reducer';
+
+export {home};
