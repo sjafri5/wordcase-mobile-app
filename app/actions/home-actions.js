154a9af3e6ad4c0187c50a13c3a762e130141e9e
sjafri5 <raza.jafri.sy@gmail.com>
2016-06-11 21:49:51 +0300

    removed logs

--- app/actions/home-actions.js
+++ app/actions/home-actions.js
@@ -1,7 +1,6 @@
 import * as types from './action-types';
 
 export function receiveWordInput(text) {
-      console.log('2', );
   return {
     type: types.RECEIVE_WORD_INPUT,
     text
--- app/reducers/home-reducer.js
+++ app/reducers/home-reducer.js
@@ -7,7 +7,6 @@ const initialState = {
 export default function homePage(state = initialState, action = {}) {
   switch (action.type) {
     case types.RECEIVE_WORD_INPUT:
-      console.log('3', );
       return Object.assign({}, state, {
         word: action.text,
       })
