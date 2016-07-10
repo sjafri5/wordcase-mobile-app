5b8e5686f155083ab5cb6e4bf7ee53dce33048d7
sjafri5 <raza.jafri.sy@gmail.com>
2016-07-09 20:34:44 -0500

    stylesheet for navbar

---
+++ app/stylesheets/nav-bar-styles.js
@@ -0,0 +1,67 @@
+'use strict';
+import React from 'react';
+import Colors from './colors';
+
+import {
+    StyleSheet,
+    PixelRatio
+} from 'react-native';
+
+var Styles = StyleSheet.create({
+  menuIconContainer: {
+    flex:1,
+    flexDirection:'row',
+    justifyContent: 'center',
+    height: 60,
+    backgroundColor: Colors.white,
+    paddingRight: 10,
+    paddingLeft: 10,
+  },
+  menuIconButton: {
+    flex:0.5,
+    flexDirection:'column',
+    justifyContent: 'center',
+    alignItems:'flex-end',
+  },
+  menuIconButton2: {
+    flex:0.5,
+    flexDirection:'column',
+    justifyContent: 'center',
+    alignItems:'flex-start',
+  },
+  menuBase: {
+    borderTopColor: Colors.gray31,
+    borderTopWidth: 1,
+    backgroundColor: Colors.slateGray,
+    flex: 1,
+  },
+  menuExpanded: {
+    flex: 1,
+    height: 250
+  },
+  sokowatchIconImage: {
+    width: 20,
+    height: 35
+  },
+  menuIconImage: {
+    width: 35,
+    height: 35
+  },
+  navBarPanel: {
+    borderBottomColor: 'white',
+    borderBottomWidth: 1,
+    height: 50,
+    flexDirection:'row',
+    alignItems:'center',
+    paddingLeft: 20,
+  },
+  panelText: {
+    paddingLeft: 20,
+    paddingRight: 20,
+    color: 'white',
+    fontSize: 20,
+    textAlign: 'left'
+  },
+})
+
+module.exports = Styles;
