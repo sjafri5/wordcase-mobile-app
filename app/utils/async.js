78c69c5e6f4a104ed651db2ef3ceb44ebab7e167
sjafri5 <raza.jafri.sy@gmail.com>
2016-08-18 00:35:13 -0500

    async module is IIFE; created fetchWordList method

--- app/utils/async.js
+++ app/utils/async.js
@@ -1,16 +1,51 @@
 import { AsyncStorage } from 'react-native';
 
-var Async = {
-  createWordList() {
+let Async = function(){ 
+  let createWordList = function() {
     AsyncStorage.getItem('wordList', (err, result) => {
       if (!result) {
         AsyncStorage.setItem('wordList', JSON.stringify({}))
       }
     })
-  },
-  cacheWord(word) {
+  };
+
+  let fetchWordList = function() {
+    return AsyncStorage.getItem('wordList', (err, result) => {
+      return result;
+    })
+  };
+
+  let cacheWord = function(word) {
    return AsyncStorage.mergeItem('wordList', JSON.stringify(word))
+  };
+
+  return {
+    createWordList,
+    cacheWord,
+    fetchWordList
   }
-}
+}()
 
 module.exports = Async;
+
+
+
+
+
+//var Async = {
+  //createWordList() {
+    //AsyncStorage.getItem('wordList', (err, result) => {
+      //if (!result) {
+        //AsyncStorage.setItem('wordList', JSON.stringify({}))
+      //}
+    //})
+  //},
+  //fetchWordList() {
+    //return AsyncStorage.getItem('wordList', (err, result) => {
+      //return result;
+    //})
+  //},
+  //cacheWord(word) {
+   //return AsyncStorage.mergeItem('wordList', JSON.stringify(word))
+  //}
+//}
