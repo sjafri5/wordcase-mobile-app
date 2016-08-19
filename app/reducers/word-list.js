cd54cbb6f49385742e135b0dbafbef6d2bcd3711
sjafri5 <raza.jafri.sy@gmail.com>
2016-08-19 00:04:58 -0500

    temporarily addressed failed spec

--- app/reducers/word-list.js
+++ app/reducers/word-list.js
@@ -3,10 +3,13 @@ import * as types from '../actions/action-types';
 const initialState = [];
 
 export default function wordList(state = initialState, action = {}) {
+  console.log('stateeeeeeeeee', state);
   switch (action.type) {
     case types.INITIALIZE_WORD_LIST:
-      return Object.assign({}, state, {
-      })
+      // TODO: fix this ship 
+      //return Object.assign({}, state, {
+      //})
+      return []
     default:
       return state;
   }
