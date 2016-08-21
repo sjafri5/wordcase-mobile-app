414ce0f4d4206e36929db90fb7841ec7e3579103
sjafri5 <raza.jafri.sy@gmail.com>
2016-08-21 00:47:02 -0500

    added custome logger that logs js consoles to crashlytics

--- android/app/src/main/java/com/wordcase/MainActivity.java
+++ android/app/src/main/java/com/wordcase/MainActivity.java
@@ -26,6 +26,7 @@ public class MainActivity extends ReactActivity {
     protected void onCreate(Bundle savedInstanceState) {
       super.onCreate(savedInstanceState);
       Fabric.with(this, new Crashlytics());
+      FLog.setLoggingDelegate(ReactNativeFabricLogger.getInstance());
     }
 
     /**
